21f5af0193b732234017e44394aa63c9875f9689
Tom-BumSuChoi <qjatn0545@gmail.com>
2026-09-23 20:28:56 +0900

    test: Testcontainers 기반 MySQL 연동 구성
    - 실제 MySQL에서만 재현되는 경합을 관찰하기 위해 H2 대신 컨테이너 사용
    - 컨테이너 포트가 실행마다 달라져 DynamicPropertySource로 접속 정보 주입

--- src/test/kotlin/kr/kro/tomchi/tomserver/TomServerApplicationTests.kt
+++ src/test/kotlin/kr/kro/tomchi/tomserver/TomServerApplicationTests.kt
@@ -2,12 +2,32 @@ package kr.kro.tomchi.tomserver
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.mysql.MySQLContainer
 
 @SpringBootTest
+@Testcontainers
 class TomServerApplicationTests {
+
+    companion object {
+        @Container
+        @JvmStatic
+        val mysql = MySQLContainer("mysql:8.0")
+
+        @DynamicPropertySource
+        @JvmStatic
+        fun datasourceProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url") { mysql.jdbcUrl }
+            registry.add("spring.datasource.username") { mysql.username }
+            registry.add("spring.datasource.password") { mysql.password }
+        }
+    }
 
     @Test
     fun contextLoads() {
-    }
 
+    }
 }
